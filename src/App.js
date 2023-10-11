207fcbb533632f634f024a348416aefd1d4f35a5
Abhishek Sharma <abhishek.sharma@simformsolutions.com>
2023-10-11 10:56:03 +0530

    Add leftSideBar

--- src/App.js
+++ src/App.js
@@ -5,14 +5,12 @@ import { ConfigProvider } from "antd";
 import theme from "./theme/Theme";
 import Header from "./components/Header/Header";
 import Home from "./pages/Home";
-import Leftsidebar from "./components/LeftSidebar/Leftsidebar";
 function App() {
   return (
     <div className="App">
       <ConfigProvider theme={theme}>
         <Header />
         <Home />
-        <Leftsidebar />
       </ConfigProvider>
     </div>
   );
